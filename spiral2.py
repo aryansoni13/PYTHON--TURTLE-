48cda0772bb8bdf8bd2e46300449efb26fd5af6f
ARYAN SONI <127400418+aryansoni13@users.noreply.github.com>
2023-05-13 12:57:41 +0530

    Add files via upload

---
+++ spiral2.py
@@ -0,0 +1,18 @@
+from turtle import *
+from colorsys import *
+bgcolor('black')
+width(2)
+seth(45)
+speed(0)
+n = 180
+
+for i in range(n):
+    c = hsv_to_rgb(i/6,i/n,1)
+    fillcolor(c)
+    begin_fill()
+    rt(90)
+    circle(i*1.2,90)
+    end_fill()
+    rt(59) 
+ht()
+done()
